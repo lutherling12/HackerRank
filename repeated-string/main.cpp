0345b3a3bbca3fea9cb75938fbf395dc591ba0e4
Luther Ling <lutherling12@gmail.com>
2021-03-13 19:21:34 -0500

    repeated string: added working solution

--- repeated-string/main.cpp
+++ repeated-string/main.cpp
@@ -1,11 +1,32 @@
 #include <bits/stdc++.h>
+#include <cstdlib>
+#include <vector>
 
 using namespace std;
 
 // Complete the repeatedString function below.
-long repeatedString(string s, long n) {
+long repeatedString(string s, long n)
+{
+    const auto length = s.size();
+    vector<bool> hasA(length);
 
+    for(auto c = 0; length > c; ++c) {
+        // record positions where A is present
+        hasA[c] = ('a' == s[c]) ? 1 : 0;
+    }
 
+    // find the number of As in repeated string using modulo arithmetic
+    const auto results = ldiv(n, length);
+    const long repeats = results.quot;
+    const long partial = results.rem;
+
+    long numberOfAs = 0;
+    for(auto pos = 0; length > pos; ++pos) {
+        numberOfAs += hasA[pos] * repeats;
+        numberOfAs += hasA[pos] * ((partial > pos) ? 1 : 0);
+    }
+
+    return numberOfAs;
 }
 
 int main()
